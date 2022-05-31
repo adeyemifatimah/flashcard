2fdedcb9b73aa539f69439dc76fad4c4674d7a98
abisoye <beesoye111@gmail.com>
2022-05-31 11:21:40 +0100

    pushing to github

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+$ (document).ready(function () {
+  $(".touch#java").click(function(){
+    $(".javascript").slideToggle(300);
+  });
+
+  $(".ball#ops").click(function(){
+    $(".operate").slideToggle(300);
+  });
+
+
+  $(".var#able").click(function(){
+    $(".show-var").slideToggle(300);
+  });
+
+
+  $(".convention#show").click(function(){
+    $(".var-convent").slideToggle(300);
+  });
+
+
+  $(".function#fun").click(function(){
+    $(".no-funct").slideToggle(300);
+  });
+
+
+  $(".method#melt").click(function(){
+    $(".show-method").slideToggle(300);
+  });
+
+
+  $(".argument#arg").click(function(){
+    $(".showargument").slideToggle(300);
+  });
+
+
+  $(".parameter#param").click(function(){
+    $(".showpara").slideToggle(300);
+  });
+
+
+  
+
+  
+
+
+  
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
